1c232646ae9e4b6d885be57a9bf844affe7a4477
Kat Lasota <lasota.kat@gmail.com>
2018-07-17 22:25:28 +0200

    Added mustache template for carousel

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
-//var Flickity = require('flickity-hash');
+'use strict';
+(function(){ 
+
+	var templateList = document.getElementById('template-product-list').innerHTML;
+	var templateItem = document.getElementById('template-carousel-cell').innerHTML;
+	
+	// Optymalizacja 	
+
+	Mustache.parse(templateItem);
+	
+	// Lista slajdow
+	
+	var listItems = '';
+	
+
+	for(var i = 0; i < productsData.length; i++){
+		console.log(productsData);
+		listItems += Mustache.render(templateItem, productsData[i]);
+	}
+	
+	var fullProductList = Mustache.render(templateList, {products: listItems});
+	
+	results.insertAdjacentHTML('beforeend', fullProductList);
+		
+})(); 
+
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
